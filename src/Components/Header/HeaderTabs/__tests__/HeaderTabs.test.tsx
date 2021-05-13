6b1dc11672498d8ede6c134101e078cfab7f367a
iownapc-af <cunninghamlogan03@gmail.com>
2021-05-13 15:56:06 -0500

    Add classes to header tabs

--- src/Components/Header/HeaderTabs/__tests__/HeaderTabs.test.tsx
+++ src/Components/Header/HeaderTabs/__tests__/HeaderTabs.test.tsx
@@ -17,6 +17,7 @@ describe('when the header tabs load', () => {
     it('should display two tabs', () => {
       expect(soteriaMount.getComponent().find('.header-tabs-button')).toHaveLength(2);
     });
+
     it('should have the 1st tab have the \'selected\' CSS', () => {
       expect(soteriaMount.getComponent().find('.header-tabs-button').at(0)
         .hasClass('header-tabs-button__selected')).toBe(true);
@@ -29,10 +30,11 @@ describe('when the header tabs load', () => {
     describe('when you click the 2nd tab', () => {
       beforeEach(async () => {
         await soteriaMount.interact((component) => {
-          component.find('header-tabs-button').at(1)
+          component.find('.header-tabs-button').at(1)
             .simulate('click');
         });
       });
+      
       it('should have the 2nd tab have the \'selected\' CSS', () => {
         expect(soteriaMount.getComponent().find('.header-tabs-button').at(1)
           .hasClass('header-tabs-button__selected')).toBe(true);

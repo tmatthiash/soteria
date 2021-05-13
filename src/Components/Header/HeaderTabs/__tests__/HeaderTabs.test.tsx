682422415f0bb5addc84cf7b0e9a9a72bbe06109
Matthias Holcombe <tmatthiash@kr.af.mil>
2021-05-12 19:36:47 -0500

    added tests for headertabs several fail (by design)

--- src/Components/Header/HeaderTabs/__tests__/HeaderTabs.test.tsx
+++ src/Components/Header/HeaderTabs/__tests__/HeaderTabs.test.tsx
@@ -14,11 +14,34 @@ describe("when the header tabs load", () => {
     ).initialize();
   });
 
-
     it("should display two tabs", () => {
+      const test = soteriaMount.getComponent().find(".header-tabs-button").at(0)
       expect(soteriaMount.getComponent().find(".header-tabs-button")).toHaveLength(2);
     })
+    it("should have the 1st tab have the 'selected' CSS", () => {
+      expect(soteriaMount.getComponent().find(".header-tabs-button").at(0)
+        .hasClass("header-tabs-button__selected")).toBe(true)
+    })
 
+  it("should have the 'selectedTab' property in redux set to 'list'", () => {
+    expect(soteriaMount.getStore().getState().tabSelected).toEqual("list");
+  })
 
+  describe("when you click the 2nd tab", () => {
+    beforeEach(async () => {
+      await soteriaMount.interact((component) => {
+        component.find("header-tabs-button").at(1)
+          .simulate("click")
+      })
+    })
+    it("should have the 2nd tab have the 'selected' CSS", () => {
+      expect(soteriaMount.getComponent().find(".header-tabs-button").at(1 )
+        .hasClass("header-tabs-button__selected")).toBe(true)
+    })
+
+    it("should have the 'selectedTab' property in redux set to 'new'", () => {
+      expect(soteriaMount.getStore().getState().tabSelected).toEqual("new");
+    })
+  })
   }
 )

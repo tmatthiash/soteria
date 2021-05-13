92ffe1ae69a870d9674e0e6bfe075bdbe8482b73
Matthias Holcombe <tmatthiash@kr.af.mil>
2021-05-13 14:31:45 -0500

    formatting fixes, and minor lint changes

--- src/Components/Header/HeaderTabs/HeaderTabs.tsx
+++ src/Components/Header/HeaderTabs/HeaderTabs.tsx
@@ -1,22 +1,31 @@
-import * as React from "react";
-import "./HeaderTabs.scss";
+import * as React from 'react';
+import './HeaderTabs.scss';
 import { useSelector } from 'react-redux';
+import classNames from 'classnames';
 import { AppState } from '../../../Store/AppState';
 
-// interface EmptyFunctionalProps {
+// interface HeaderTabProps {
 //   someNumberProp: number;
 // }
 
 export const HeaderTabs = () => {
   const tabSelected = useSelector((state: AppState) => state.tabSelected);
 
+
   return (
-    <div className="header-tabs">
-    <button type="button" className="header-tabs-button" onClick={()=>{}}>
-      IDF Incident List {tabSelected}
-    </button>
-    <button type="button" className="header-tabs-button" onClick={()=>{}}>
-      Create New Incident
-    </button>
-  </div>
-)};
+    <div className='header-tabs'>
+      <button type='button'
+              className={classNames('header-tabs-button', 'primary-button')}
+              onClick={() => {
+              }}>
+        IDF Incident List {tabSelected}
+      </button>
+      <button type='button'
+              className={classNames('header-tabs-button', 'primary-button')}
+              onClick={() => {
+              }}>
+        Create New Incident
+      </button>
+    </div>
+  );
+};
--- src/Components/Header/HeaderTabs/__tests__/HeaderTabs.test.tsx
+++ src/Components/Header/HeaderTabs/__tests__/HeaderTabs.test.tsx
@@ -1,47 +1,46 @@
-import React from "react";
+import React from 'react';
 import { SoteriaMount } from '../../../../Test/SoteriaMount';
 import { HeaderTabs } from '../HeaderTabs';
 
-describe("when the header tabs load", () => {
-  let soteriaMount: SoteriaMount;
+describe('when the header tabs load', () => {
+    let soteriaMount: SoteriaMount;
 
-  beforeEach(async () => {
-
-    soteriaMount = await new SoteriaMount(
-      (
-        <HeaderTabs />
-      )
-    ).initialize();
-  });
-
-    it("should display two tabs", () => {
-      const test = soteriaMount.getComponent().find(".header-tabs-button").at(0)
-      expect(soteriaMount.getComponent().find(".header-tabs-button")).toHaveLength(2);
-    })
-    it("should have the 1st tab have the 'selected' CSS", () => {
-      expect(soteriaMount.getComponent().find(".header-tabs-button").at(0)
-        .hasClass("header-tabs-button__selected")).toBe(true)
-    })
-
-  it("should have the 'selectedTab' property in redux set to 'list'", () => {
-    expect(soteriaMount.getStore().getState().tabSelected).toEqual("list");
-  })
-
-  describe("when you click the 2nd tab", () => {
     beforeEach(async () => {
-      await soteriaMount.interact((component) => {
-        component.find("header-tabs-button").at(1)
-          .simulate("click")
-      })
-    })
-    it("should have the 2nd tab have the 'selected' CSS", () => {
-      expect(soteriaMount.getComponent().find(".header-tabs-button").at(1 )
-        .hasClass("header-tabs-button__selected")).toBe(true)
-    })
 
-    it("should have the 'selectedTab' property in redux set to 'new'", () => {
-      expect(soteriaMount.getStore().getState().tabSelected).toEqual("new");
-    })
-  })
+      soteriaMount = await new SoteriaMount(
+        (
+          <HeaderTabs />
+        )
+      ).initialize();
+    });
+
+    it('should display two tabs', () => {
+      expect(soteriaMount.getComponent().find('.header-tabs-button')).toHaveLength(2);
+    });
+    it('should have the 1st tab have the \'selected\' CSS', () => {
+      expect(soteriaMount.getComponent().find('.header-tabs-button').at(0)
+        .hasClass('header-tabs-button__selected')).toBe(true);
+    });
+
+    it('should have the \'selectedTab\' property in redux set to \'list\'', () => {
+      expect(soteriaMount.getStore().getState().tabSelected).toEqual('list');
+    });
+
+    describe('when you click the 2nd tab', () => {
+      beforeEach(async () => {
+        await soteriaMount.interact((component) => {
+          component.find('header-tabs-button').at(1)
+            .simulate('click');
+        });
+      });
+      it('should have the 2nd tab have the \'selected\' CSS', () => {
+        expect(soteriaMount.getComponent().find('.header-tabs-button').at(1)
+          .hasClass('header-tabs-button__selected')).toBe(true);
+      });
+
+      it('should have the \'selectedTab\' property in redux set to \'new\'', () => {
+        expect(soteriaMount.getStore().getState().tabSelected).toEqual('new');
+      });
+    });
   }
-)
+);

e98e4f93439b9bc99da04c862276c1f9623218e4
Matthias Holcombe <tmatthiash@kr.af.mil>
2021-05-19 08:40:38 -0500

    Finished home page component

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import './HomePage.scss';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../Store/AppState';
 import { ListView } from '../../Components/ListView/ListView';
+import { NewView } from '../../Components/NewView/NewView';
 
 
 export const HomePage = () => {
@@ -12,6 +13,9 @@ export const HomePage = () => {
     <div className='home-page'>
       {
         tabSelected === 'list' ? (<ListView />) : null
+      }
+      {
+        tabSelected === 'new' ? (<NewView />) : null
       }
     </div>
   );
--- src/Pages/HomePage/__tests__/HomePage.test.tsx
+++ src/Pages/HomePage/__tests__/HomePage.test.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { SoteriaMount } from '../../../Test/SoteriaMount';
 import { HomePage } from '../HomePage';
 
-describe( 'when the home page loads', () => {
+describe('when the home page loads', () => {
   let soteriaMount: SoteriaMount;
 
   beforeEach(async () => {
 
     soteriaMount = await new SoteriaMount(
       (
-        <HomePage/>
+        <HomePage />
       )
     ).initialize();
   });
@@ -17,6 +17,19 @@ describe( 'when the home page loads', () => {
   it('should display the list view', () => {
     expect(soteriaMount.getComponent().find('.list-view').exists()).toBe(true);
     expect(soteriaMount.getComponent().find('.new-view').exists()).toBe(false);
-  })
+  });
 
+  describe('when new view tab is selected', () => {
+    beforeEach(async () => {
+      await soteriaMount.dispatch({
+        type: 'SET_TAB_SELECTED',
+        tabSelected: 'new',
+      });
+    });
+
+    it('should display the new view', () => {
+      expect(soteriaMount.getComponent().find('.new-view').exists()).toBe(true);
+      expect(soteriaMount.getComponent().find('.list-view').exists()).toBe(false);
+    });
+  });
 });
